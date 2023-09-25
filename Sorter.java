bfef5d4f9e2624ae4290dc0c2ab3f3d8e6717df3
Escyth <x.shinki.x@gmail.com>
2023-09-25 17:30:33 +0300

    sort calls selectionsort

--- Sorter.java
+++ Sorter.java
@@ -1,6 +1,6 @@
 public class Sorter {
 
-    public void selectionSort(int[] arr) {
+    public static void selectionSort(int[] arr) {
         int n = arr.length;
 
         for (int i = 0; i < n - 1; i++) {
@@ -13,6 +13,10 @@ public class Sorter {
             arr[min] = arr[i];
             arr[i] = temp;
         }
+    }
+
+    public static void sort(int[] arr) {
+        selectionSort(arr);
     }
 
 }
